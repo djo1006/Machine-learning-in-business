b1ef4025aacf99620fae0f4dbb512fa03e9feecf
djo1006 <63151514+djo1006@users.noreply.github.com>
2021-05-31 14:11:40 +0300

    course_work

--- course_work/run_docker.sh
+++ course_work/run_docker.sh
@@ -1,1 +1,4 @@
-docker run -d -p 8180:8180 -p 8181:8181 djo1006/german_credit_data_pipeline -v ./models:/app/app/models
+docker run -d -p 8180:8180 -p 8181:8181 djo1006/german_credit_data_pipeline -v D:/tmp:/app/app/models
+# docker run -d -p 8180:8180 -p 8181:8181 djo1006/german_credit_data_pipeline cp D:/tmp/german_credit_data_pipeline.dill :/app/app/models
+# Если при старте по логам не видит, скопируй ;), проблема где то на стыке win b nix, возможно нужно расшарить
+# docker cp D:/tmp/german_credit_data_pipeline.dill 4de22b59a959:/app/app/models
